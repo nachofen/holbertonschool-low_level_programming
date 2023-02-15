737026dd649ca9fb54b57f17212db344307a3aa9
nachofen <zorba037@gmail.com>
2023-02-15 09:55:07 -0800

    4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-int main(void)
+int main(void)/* The main function serves as the starting point for program execution.*/
 {
 puts("\"Programming is like building a multilingual puzzle"\n);
 return (0);
