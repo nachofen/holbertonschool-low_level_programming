03399bf50cea3585118d55c8305c1432b840fbd8
nachofen <zorba037@gmail.com>
2023-02-15 11:31:18 -0800

    6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -11,10 +11,10 @@ int IntType;
 long int longintType;
 long long int longlongintType;
 float floatType;
-printf("Size of char: %ld bytes\n", sizeof(charType));
-printf("Size of int: %ld bytes\n", sizeof(IntType));
-printf("Size of long int: %ld bytes\n", sizeof(longintType));
-printf("Size of long long int: %ld byte\n", sizeof(longlongintType));
-printf("Size of char: %ld bytes\n", sizeof(floatType));
+printf("Size of char: %ld byte(s)\n", sizeof(charType));
+printf("Size of int: %ld byte(s)\n", sizeof(IntType));
+printf("Size of long int: %ld byte(s)\n", sizeof(longintType));
+printf("Size of long long int: %ld byte(s)\n", sizeof(longlongintType));
+printf("Size of char: %ld byte(s)\n", sizeof(floatType));
 return (0);
 }
