35d2f3f3b1a4af424d600dd031a85e8868eee9c0
nachofen <zorba037@gmail.com>
2023-02-16 11:23:07 -0800

    5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - This is a description
+ *
+ * Return: 0 works
+ */
+int main(void)
+{
+	int pot = 0;
+
+	while (pot < 10)
+	{
+		printf("%u", pot);
+		pot++;
+	}
+
+return (0);
+}
