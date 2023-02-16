00e24ede52040c9b4e263442b77e2e54a6bca44e
nachofen <zorba037@gmail.com>
2023-02-16 09:28:02 -0800

    2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - This is a description
+ *
+ * Return: 0 works
+ */
+int main(void)
+{
+char ch = 'a';
+while (ch <= 'z')
+{
+	putchar(ch);
+	ch++;
+}
+return (0);
+}
