d9a2536ca41751934c32cfaae53b327416d777b9
nachofen <zorba037@gmail.com>
2023-02-17 09:31:41 -0800

    2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - This is a description
+ *
+ */
+void print_alphabet_x10(void)
+{
+int i;
+
+char c;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar('c');
+		}
+	}
+}
