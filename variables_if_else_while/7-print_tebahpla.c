7facfead2ce384f1cba58386440c83f741e1496d
nachofen <zorba037@gmail.com>
2023-02-16 17:41:59 -0800

    7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *  * main - This is a description
+ *   *
+ *    * Return: 0 works
+*/
+int main(void)
+{
+	char pot = 'z';
+
+	while (pot >= 'a')
+	{
+		putchar(pot);
+		pot--;
+	}
+	printf("\n");
+	return (0);
+}
