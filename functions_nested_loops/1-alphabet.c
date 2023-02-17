70e1d16f9c090df7c59479e0904bfffc32418a85
nachofen <zorba037@gmail.com>
2023-02-17 08:52:21 -0800

    1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - This is a description
+ *
+ * Return: 0 works
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c < 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
