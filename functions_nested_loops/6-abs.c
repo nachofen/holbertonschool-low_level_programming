5069213349f60d231e5bef1ff3be35185a24cfdd
nachofen <zorba037@gmail.com>
2023-02-17 11:33:56 -0800

    6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _abs - This is a description
+ * @c: input char
+ * Return: 1 if is lowercase or 0 if is uppercase
+ */
+int _abs(int c)
+{
+	int c2;
+
+	if (c > 0)
+	{
+		_putchar(c);
+	}
+	else if (c < 0)
+	{
+		c2 = c * -1;
+		_putchar(c2);
+	}
+	else
+	{
+		_putchar('0');
+	}
+
+	return (0);
+}
