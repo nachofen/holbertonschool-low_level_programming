29b3b0228fa4318289253742b01baaef4755e301
nachofen <zorba037@gmail.com>
2023-02-18 05:57:50 -0800

    9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -9,6 +9,7 @@ void times_table(void)
 
 	for (row1 = 0; row1 <= 9; row1++)
 	{
+	_putchar('0');
 	_putchar(',');
 	_putchar(' ');
 		for (col1 = 1; col1 <= 9; col1++)
@@ -16,7 +17,7 @@ void times_table(void)
 			r = (row1 * col1);
 			if (r >= 0)
 			{
-				_putchar((r / 10) + 48);
+				
 			}
 			else
 			{
