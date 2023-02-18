609b6618fc2f4df9bb014c9b6cb6f94cd0e2660e
nachofen <zorba037@gmail.com>
2023-02-18 04:03:58 -0800

    10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - This is a description
+ * @num1: number one
+ * @num2: number two
+ * Return: int resultado
+ *
+ */
+int add(int num1, int num2)
+{
+	int resultado;
+
+	resultado = num1 + num2;
+
+	return (resultado);
+}
