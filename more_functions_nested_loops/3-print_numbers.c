1e0c6edf14418ede546b76c1d6deb5ae2ae5d8ab
nachofen <zorba037@gmail.com>
2023-02-23 05:42:22 -0800

    3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_numbers - A function that multiplies two integers
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		_putchar (i);
+		_putchar ('\n');
+	}
+}
