990a188264c7f8acff812d27a098ace49f9db532
nachofen <zorba037@gmail.com>
2023-02-23 08:20:37 -0800

    5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * more_numbers - A function that multiplies two integers
+ */
+void more_numbers(void)
+{
+	int i1, i2;
+
+	for (i1 = 0; i1 < 10; i1++)
+	{
+		for (i2 = 1; i2 <= 14; i2++)
+		{
+			if (i2 != 0)
+			{
+				_putchar((i2 / 10) + 48);
+			}
+				_putchar((i2 % 10) + 48);
+		}
+	_putchar ('\n');
+	}
+}
