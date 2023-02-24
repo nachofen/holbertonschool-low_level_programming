6ab3098b4d23106ad7142dd4282ec6deefa51b12
nachofen <zorba037@gmail.com>
2023-02-24 07:29:39 -0800

    9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - FIZZBUZZ
+ */
+void main(void)
+{
+	int i;
+
+	for (i = 0; i < 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf(i);
+	}
+}
