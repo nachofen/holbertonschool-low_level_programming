b5b13cf13765f1da88512f2557dc7dee102bbfc3
nachofen <zorba037@gmail.com>
2023-02-27 09:03:47 -0800

    4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_rev - a function that print a string in reverse
+ * @s: the char pointer
+ */
+void print_rev(char *s)
+{
+	int i = 0, length = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	for (length = i; length >= 0; length--)
+	{
+		_putchar(s[length]);
+	}
+	_putchar('\n');
+}
