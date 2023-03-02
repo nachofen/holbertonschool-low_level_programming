9bd2ad6fd9bfca4cba4f175ba71882ed7614d636
nachofen <zorba037@gmail.com>
2023-03-02 12:18:43 -0800

    7

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * cap_string - a function that print a string in reverse
+ * @leet: the char pointer
+ * Return: a
+ */
+char *leet(char *string)
+{
+	int contadorfuera = 0, contadordentro = 0;
+	char letras[] = "aeotl";
+	char numeros[5] = "43071";
+
+	while (string[contadorfuera])
+	{
+		while (letras[contadordentro])
+		{
+			if (string[contadorfuera] == letras[contadordentro])
+			{
+				string[contadorfuera] = numeros[contadordentro];
+			}
+			contadordentro++;
+		}
+		contadorfuera++;
+		contadordentro = 0;
+	}
+	return (string);
+}
