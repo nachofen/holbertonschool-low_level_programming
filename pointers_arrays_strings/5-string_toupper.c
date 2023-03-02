af57585f0a859a8d9d811584aa58f78e31fbcc59
nachofen <zorba037@gmail.com>
2023-03-02 06:14:46 -0800

    5

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * string_toupper - a function that print a string in reverse
+ * @asterisco: the char pointer
+ * Return: a
+ */
+char *string_toupper(char *asterisco)
+{
+	int length = 0;
+
+	while (asterisco[length])
+	{
+		if (asterisco[length] >= 'a' && asterisco[length] <= 'z')
+		{
+			asterisco[length] = asterisco[length] - 32;
+		}
+		length++;
+	}
+	return(asterisco);
+}
