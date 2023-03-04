cc0e8a90183830bff858f8d5bd427730f86e6718
nachofen <zorba037@gmail.com>
2023-03-03 16:30:41 -0800

    4

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
- * _strbrk - a function that print a string in reverse
+ * _strpbrk - a function that print a string in reverse
  * @s: the char pointer
  * @accept: the chars to find
  * Return: a
  */
 char *_strpbrk(char *s, char *accept)
 {
-	int contador = 0, contador2 = 0, encontro = 0;
+	int contador = 0, contador2 = 0;
+	char *puntero;
 
 	while (s[contador])
 	{
@@ -15,17 +16,13 @@ char *_strpbrk(char *s, char *accept)
 		{
 			if (s[contador] == accept[contador2])
 			{
-				encontro = 1;
-				return (s + contador);		
+				puntero = &s[contador];
+				return (puntero);
 			}
 			contador2++;
 		}
 		contador++;
 		contador2 = 0;
-	}
-	if (encontro == 0)
-	{
-		return (s + contador);
 	}
 	return (0);
 }
