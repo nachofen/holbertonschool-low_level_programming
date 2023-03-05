8181a2cfd758d62c42a97ca4932872bee7b452b5
nachofen <zorba037@gmail.com>
2023-03-05 11:17:23 -0800

    5

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -7,29 +7,24 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int contador = 0, contador2 = 0, contador3 = 0, contencontre = 0, letras = 0;
+	int contador = 0, contador2 = 0, contencontre = 0, letras = 0;
 	char *puntero;
 
 	while (needle[contador])
 	{
 		contador++;
 	}
-	while (haystack[contador3])
-	{
-		contador3++;
-	}
 	while (haystack[contador2])
-	{	
-		while (haystack[contador2] == needle[contencontre])
+	{
+		while (haystack[contador2 + contencontre] == needle[contencontre])
 		{
-			contencontre++;
 			letras++;
-			
 			if (contador == letras)
 			{
-				puntero = &haystack[contador2 - letras + 1];
+				puntero = &haystack[contador2];
 				return (puntero);
 			}
+			contencontre++;
 		}
 		contador2++;
 		contencontre = 0;
