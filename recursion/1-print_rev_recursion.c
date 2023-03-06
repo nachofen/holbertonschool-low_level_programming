3a7752c933b1a3db0b84ec271abe105353363a7a
nachofen <zorba037@gmail.com>
2023-03-06 06:31:02 -0800

    1

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
- * _strlen_recursion - a function that prints a string in reverse.
+ * _print_rev_recursion - a function that prints a string in reverse.
  * @s: An input string to printing
  * Return: Nothing
  */
 
-int _strlen_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
 	{
 		return;
 	}
-	_puts_recursion(s + 1);
+	_print_rev_recursion(s + 1);
 	_putchar(*s);
 }
