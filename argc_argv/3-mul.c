47ec6b696b07d27270e7b49d28a5cd5f4861d112
nachofen <zorba037@gmail.com>
2023-03-10 07:21:48 -0800

    3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -9,12 +9,15 @@
  */
 int main(int argc, char *argv[])
 {
-	int num1 = atoi(argv[1]), num2 = atoi(argv[2]);
-	int resultado = num1 * num2;
+	int resultado = 0, num1 = 0, num2 = 0;
 
 	if (argc == 3)
 	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		resultado = num1 * num2;
 		printf("%d\n", resultado);
+		return (0);
 	}
 	else
 	{
