e11629e748963a9f8072d6dfcf8ce8b46296398b
nachofen <zorba037@gmail.com>
2023-03-14 12:03:03 -0700

    1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,64 +1,16 @@
 #include <stdlib.h>
 #include "main.h"
 
-/**
- ** _strncat - a function that concatenates two strings 
- ** @dest: the char pointer
- ** @src: the first string
- ** @n : the size of second string to be added
- ** Return: a
- **/
-char *_strncat(char *dest, char *src, int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int length = 0, l2 = 0;
-	char *auxi = dest;
+	unsigned int i;
 
-	if (n >= 0)
+	for (i = 0; i < n; i++)
 	{
-		while (dest[length])
-		{
-			length++;
-		}
-		while (src[l2] && n > 0)
-		{
-			dest[length] = src[l2];
-			length++;
-			l2++;
-			n--;
-		}
+		dest[i] = src[i];
 	}
-	else
-	{
-		auxi[length] = '\0';
-	}
-	return (auxi);
-}
-/**
- ** _strcpy - a function that copy a string
- ** @dest: the destiny pointer
- ** @src: src pointer
- ** Return: a pointer to copied string
- **/
-char *_strcpy(char *dest, char *src)
-{
-	int length = 0;
-
-	while (src[length])
-	{
-		dest[length] = src[length];
-		length++;
-	}
-	dest[length] = '\0';
 	return (dest);
 }
-
-/**
- * string_nconcat - a function that concatenates two strings
- * @s1: first string
- * @s2: second string
- * @n: bytes of s2 to be added to s1
- * Return: pointer to string
- */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *pointer;
@@ -87,7 +39,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		exit(1);
 	}
-	_strcpy(pointer, s1);
-	_strncat(pointer, s2, n);
+	_memcpy(pointer, s1, s1_len);
+	_memcpy(pointer + s1_len, s2, n);
+	pointer[s1_len + n] = '\0';
 	return (pointer);
 }
