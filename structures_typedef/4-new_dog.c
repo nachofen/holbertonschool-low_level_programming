b8e034b5ca85bb35b66798b3a0babe14d715d418
nachofen <zorba037@gmail.com>
2023-03-20 12:15:19 -0700

    4

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * _strcpy - a function that print a string in reverse
+ * @dest: the char pointer
+ * @src: a
+ * Return: a
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int length = 0;
+
+	while (src[length])
+	{
+		dest[length] = src[length];
+		length++;
+	}
+	dest[length] = '\0';
+	return (dest);
+}
+
+/**
+ * new_dog - ini dog
+ * @name: pointer
+ * @age: age
+ * @owner: pointer
+ * Return: Always 0.
+ */
+
+
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *pdog;
+	char *copyname, *copyowner;
+	int contname = 0, contowner = 0;
+
+	pdog = malloc(sizeof(dog_t));
+
+	if (pdog == NULL)
+	{
+		return (NULL);
+	}
+	while (name[contname])
+	{
+		contname++;
+	}
+	while (owner[contowner])
+	{
+		contowner++;
+	}
+	copyname = malloc(contname + 1);
+	copyowner = malloc(contowner + 1);
+
+	_strcpy(copyname, name);
+	_strcpy(copyowner, owner);
+	pdog->name = copyname;
+	pdog->age = age;
+	pdog->owner = copyowner;
+
+	return (pdog);
+}
