e797f00a21f99a45c203c3c84e267be1c2f1ba54
nachofen <zorba037@gmail.com>
2023-03-21 07:59:38 -0700

    0

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,11 +1,16 @@
 #include "function_pointers.h"
 #include <stdlib.h>
 
+/**
+ * print_name - print the name
+ * @name: name to print
+ * @f: function to use
+ */
 void print_name(char *name, void (*f)(char *))
 {
-    if (name != NULL && f != NULL)
-    {
-        f(name);
-    }
+	if (name != NULL && f != NULL)
+	{
+		f(name);
+	}
 }
 
