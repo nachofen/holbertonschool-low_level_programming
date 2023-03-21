70d4deedc3c5ac436d238ddf7a2739d6d9224f21
nachofen <zorba037@gmail.com>
2023-03-21 13:19:57 -0700

    3

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -5,15 +5,15 @@
 int (*get_op_func(char *s))(int, int)
 {
 op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i = 0;
-	
+	{"+", op_add},
+	{"-", op_sub},
+	{"*", op_mul},
+	{"/", op_div},
+	{"%", op_mod},
+	{NULL, NULL}
+};
+int i = 0;
+
 	while (ops[i].f != NULL)
 	{
 		if (strcmp((ops[i].op), s) == 0)
@@ -23,6 +23,6 @@ op_t ops[] = {
 		i++;
 	}
 	printf("Error\n");
-	exit (99);
+	exit(99);
 
 }
--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,21 +1,51 @@
 #include "3-calc.h"
-	
+
+/**
+* op_add - add function
+* @a: num1
+* @b: num2
+* Return: int
+*/
 int op_add(int a, int b)
 {
 	return (a + b);
 }
+/**
+* op_sub - add function
+* @a: num1
+* @b: num2
+* Return: int
+*/
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
+/**
+* op_mul - add function
+* @a: num1
+* @b: num2
+* Return: int
+*/
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
+/**
+* op_div - add function
+* @a: num1
+* @b: num2
+* Return: int
+*/
 int op_div(int a, int b)
 {
 	return (a / b);
 }
+/**
+* op_mod - add function
+* @a: num1
+* @b: num2
+* Return: int
+*/
 int op_mod(int a, int b)
 {
 	return (a % b);
