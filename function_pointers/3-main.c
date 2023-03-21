7b417f562c573819be7e3260b20293ecd5f1d3ac
nachofen <zorba037@gmail.com>
2023-03-21 13:22:33 -0700

    3

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -8,15 +8,16 @@ int main(int argc, char *argv[])
 	{
 		int num1 = atoi(argv[1]), num2 = atoi(argv[3]);
 		int (*aux)(int, int);
+
 		aux = get_op_func(argv[2]);
 		if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
 		{
 			printf("Error\n");
-			exit (100);
+			exit(100);
 		}
-		printf("%d\n",aux(num1,num2));
+		printf("%d\n", aux(num1, num2));
 		return (0);
 	}
 	printf("Error\n");
-	exit (98);
+	exit(98);
 }
