62873a6d66353ae342c75efa8d43a022e0173e0f
nachofen <zorba037@gmail.com>
2023-03-23 08:31:15 -0700

    1

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -18,7 +18,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (; i < n; i++)
 	{
 		printf("%d", va_arg(numbers, int));
-		if (separator != NULL && i < n-1)
+		if (separator != NULL && i < n - 1)
 		{
 			printf("%s", separator);
 		}
