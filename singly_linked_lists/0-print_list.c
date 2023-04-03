af8e6b174607f608a906aa1dcb8ada0a193fe1e0
nachofen <zorba037@gmail.com>
2023-04-03 08:06:32 -0700

    0

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * print_list - this function prints the content of str and count the number of
+ * nodes
+ * @h: the structure
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t counter = 0;
+
+		while (h != NULL)
+		{
+			if (h->str == NULL)
+			{
+				printf("[0] (nil)");
+			}
+			else
+			{
+				printf("%s", h->str);
+			}
+			counter++;
+			h = h->next;
+		}
+	return (counter);
+}
