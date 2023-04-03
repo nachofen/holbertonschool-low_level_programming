0cf4616282e2b98ca635c8cf929710e68810f421
nachofen <zorba037@gmail.com>
2023-04-03 11:40:31 -0700

    4

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
+#include "lists.h"
+/**
+ * add_node_end - this function return the pointer to new node added at the
+ * end
+ * @head: a pointer to head pointer
+ * @str: the string to duplicate
+ * Return: the number of nodes
+ */
+void free_list(list_t *head)
+{
+	list_t *temp = head;
+
+	while (temp != NULL)
+	{
+		temp = temp->next;
+		free(head);
+		head = temp;
+	}
+}
