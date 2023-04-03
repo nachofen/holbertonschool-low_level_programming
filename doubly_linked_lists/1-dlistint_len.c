9d6ac01539bccbec9f26b35994b09cd06c76aae2
nachofen <zorba037@gmail.com>
2023-04-03 13:01:35 -0700

    1

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * dlistint_len - this function counts the number of nodes
+ * @h: the structure
+ * Return: number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t counter = 0;
+
+		while (h != NULL)
+		{
+			counter++;
+			h = h->next;
+		}
+	return (counter);
+}
