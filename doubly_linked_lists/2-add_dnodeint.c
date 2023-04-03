334a103530afe0190708e3635477bd01e452996a
nachofen <zorba037@gmail.com>
2023-04-03 13:47:20 -0700

    2

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -14,17 +14,18 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *tmp;
 
 	tmp = malloc(sizeof(dlistint_t));
-
+	
+	
 	if (tmp == NULL)
 	{
 		free(tmp);
 		return (NULL);
 	}
+	
 	tmp->n = n;
-
+	tmp->prev = NULL;
 	tmp->next = *head;
 	*head = tmp;
-
 
 	return (*head);
 }
