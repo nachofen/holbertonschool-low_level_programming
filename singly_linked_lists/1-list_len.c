eed42ae0bfda9ad22fcd06b2847133d7c4f3afb1
nachofen <zorba037@gmail.com>
2023-04-03 09:02:12 -0700

    1

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * list_len - this function count the number of nodes
+ * @h: the structure
+ * Return: the number of nodes
+ */
+size_t list_len(const list_t *h)
+{
+	size_t counter = 0;
+
+		while (h != NULL)
+		{
+			printf("[%d] %s\n", h->len, h->str);
+			counter++;
+			h = h->next;
+		}
+	return (counter);
+}
