19ba2f119343148f3b131949f3110d673ed35abf
nachofen <zorba037@gmail.com>
2023-04-09 11:33:53 -0700

    0

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * _strlen - calculates the length of the string
+ * @s: input
+ * Return: length of string
+ */
+unsigned int _strlen(const char *s)
+{
+	unsigned int i;
+
+	while (s[i])
+	{
+		i++;
+	}
+
+	return (i);
+}
+
+/**
+ * binary_to_uint - converts a string of binaries to a decimal number
+ * @b: string to convert
+ * Return: the decimal number
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int total = 0, exp = 1, tmp = 0;
+	int i = 0;
+
+	if (b == NULL)
+		return (0);
+	for (i = _strlen(b) - 1; b[i]; i--)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+
+		tmp = b[i] + '0';
+		total = total + (tmp * exp);
+		exp = exp * 2;
+	}
+	return (total);
+}
---
+++ bit_manipulation/main.h
@@ -0,0 +1,11 @@
+
+#ifndef main_h
+#define main_h
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
+#endif
