5947173fa124a8b07da46580f7351791806ee353
nachofen <zorba037@gmail.com>
2023-04-09 14:48:28 -0700

    0

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -29,7 +29,7 @@ unsigned int binary_to_uint(const char *b)
 
 	if (b == NULL)
 		return (0);
-	for (i = _strlen(b) - 1; b[i]; i--)
+	for (i = _strlen(b) - 1; i >= 0; i--)
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
