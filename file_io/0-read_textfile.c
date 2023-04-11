5d42f8136e8c5a2d7d221209b6feb3fdfcd00561
nachofen <zorba037@gmail.com>
2023-04-11 09:34:18 -0700

    0

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * read_textfile - a
+ * @filename: pointer to file
+ * @letters: number of letters
+ * Return: the number of chars
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int filepointer;
+	char *buffer;
+	ssize_t readn;
+
+	if (filename == NULL)
+		return (0);
+	filepointer = open(filename, O_RDONLY);
+
+	buffer = malloc(sizeof(char) * (letters + 1));
+	if (buffer == NULL)
+		return (-1);
+
+	/**
+	* buffer pointer to string
+	* size = sizeof(data) the size of the elements that will read
+	* count = letters the number of elements that will read
+	* stream = pointer to the file that will read
+	*/
+	readn = read(filepointer, buffer, letters);
+	write(STDOUT_FILENO, buffer, readn);
+
+	if (write(STDOUT_FILENO, buffer, readn) != readn)
+	{
+		free(buffer);
+		return (0);
+	}
+	free(buffer);
+	close(filepointer);
+
+	return (readn);
+}
---
+++ file_io/main.h
@@ -0,0 +1,11 @@
+#ifndef main_h
+#define main_h
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
