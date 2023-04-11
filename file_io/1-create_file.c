67cedfc05b21cbcca8d8b2aaacc8a5c0fc7959bf
nachofen <zorba037@gmail.com>
2023-04-11 12:34:10 -0700

    1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -35,6 +35,9 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
+	if (text_content == NULL)
+		text_content = "";
+
 	txtlen = _strlen(text_content);
 	fdwrite = write(fdopen, text_content, txtlen);
 
