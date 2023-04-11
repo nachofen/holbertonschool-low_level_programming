e4853acc93cf5fe48192f68e470c5b51b9dc5ca2
nachofen <zorba037@gmail.com>
2023-04-11 12:25:08 -0700

    1

---
+++ file_io/1-create_file.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * _strlen - a function that tells the length of a string
+ * @s: the char pointer
+ * Return: int
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
+
+/**
+ * create_file - a
+ * @filename: pointer to file
+ * @text_content: number of letters
+ * Return: 1 on success
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fdopen, txtlen, fdwrite;
+
+	fdopen = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+
+	if (fdopen == -1)
+	{
+		return (-1);
+	}
+	txtlen = _strlen(text_content);
+	fdwrite = write(fdopen, text_content, txtlen);
+
+	close(fdopen);
+	if (fdwrite == -1)
+		return (-1);
+
+	return (1);
+}
