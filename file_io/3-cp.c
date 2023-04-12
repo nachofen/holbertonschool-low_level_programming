d094fa14a407c1f8d4adcf91af9e0a21e89db4b6
nachofen <zorba037@gmail.com>
2023-04-12 10:23:25 -0700

    3

---
+++ file_io/3-cp.c
@@ -0,0 +1,59 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(int ac, char **av)
+{
+	char buffer [1024];
+	int open1, open2, read1, write1, close1;
+
+	if (ac != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	open1 = open(av[1], O_RDONLY);
+	if (open1 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+	open2 = open(av[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
+	if (open2 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		close(open1);
+		exit(99);
+	}
+	while ((read1 = read(open1, buffer, 1024)) >= 1)
+	{
+		write1 = write(open2, buffer, read1);
+		if(read1 == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+			exit(98);
+		}
+		if (write1 == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+			exit(99);
+		}
+	}
+	close1 = close(open1);
+	if (close1 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d", open1);
+		exit(100);
+	}
+	close1 = close(open2);
+	if (close1 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d", open2);
+		exit(100);
+	}
+	return(0);	
+}
+
