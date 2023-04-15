b342b73ab4e6b01bee2f9decc758035916a0901f
nachofen <zorba037@gmail.com>
2023-04-15 14:17:25 -0700

    0

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,36 @@
+#include "hash_tables.h"
+
+
+/**
+ * hash_table_create - this function creates a hashtable 
+ * @size: The size of the array
+ * Each cell of this array is a pointer to the first node of a linked list,
+ * because we want our HashTable to use a Chaining collision handling
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table;
+	unsigned long int counter;
+
+	if (size <= 0)
+		return (NULL);
+
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
+		return (NULL);
+
+	table->size = size;
+	table->array = malloc(size * sizeof(hash_node_t));
+	if (table->array == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
+	for (counter = 0; counter < size; counter++)
+	{
+		table->array[counter] = NULL;
+	}
+	return (table);
+
+}
