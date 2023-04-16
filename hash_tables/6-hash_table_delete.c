7c942d80da38f59d74dedcd2ef647967c2b0dcc8
nachofen <zorba037@gmail.com>
2023-04-16 15:58:50 -0700

    6

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -16,7 +16,7 @@ void hash_table_delete(hash_table_t *ht)
 		free(ht);
 		return;
 	}
-	for (index = 0; index < ht->size; index++, ht->size--)
+	for (index = 0; index < ht->size; index++)
 	{
 		tmp = ht->array[index];
 		while (tmp != NULL)
