b6fae982a14ac0da0981b25621c803d251412af2
nachofen <zorba037@gmail.com>
2023-04-16 15:40:00 -0700

    6

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+/**
+ * hash_table_delete - this function delete given hashtable
+ * @ht: hashtable to delete
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int index;
+	hash_node_t *tmp, *backup;
+
+	if (ht == NULL)
+		return;
+	for (index = 0; index < ht->size; index++)
+	{
+		tmp = ht->array[index];
+		if (tmp != NULL)
+		{
+			backup = tmp;
+			tmp = tmp->next;
+			free(backup->key);
+			free(backup->value);
+			free(backup);
+		}
+	}
+
+}
